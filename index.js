b1902d6658f1ad5c674710f41f4df22641362a54
Iqra Masroor <masrooriqra@gmail.com>
2020-02-17 20:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // Code your solution in this file.
-function lowerCaseDrivers(drivers){
- return list.map(function(driver){
-   return driver.toLowerCase();
- }) 
+  
+function lowerCaseDrivers(list) {
+  return list.map(function(driver) {
+    return driver.toLowerCase();
+  });
 }
 
-function nameToAttributes(){
-  
+function nameToAttributes(list) {
+  return list.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
 }
 
-function attributesToPhrase(){
-  
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
