a79190a810faaca24661477bd16a42f82f1e3fb6
Iqra Masroor <masrooriqra@gmail.com>
2020-02-17 20:58:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+ return list.map(function(driver){
+   return driver.toLowerCase();
+ }) 
+}
+
+function nameToAttributes(){
+  
+}
+
+function attributesToPhrase(){
+  
+}
